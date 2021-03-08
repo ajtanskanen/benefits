2a0ac6781aa76243830892c7a1188940d7ec5d4e
Antti Tanskanen <ajtanskanen@users.noreply.github.com>
2021-03-08 20:32:22 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,34 +1,11 @@
 import fnmatch
 from setuptools import find_packages, setup, Extension
 from setuptools.command.build_py import build_py as build_py_orig
-from Cython.Build import cythonize
 
 
 extensions = [
     Extension('fin_benefits.*', ['fin_benefits/benefits.py']),
 ]
-cython_excludes = ['**/__init__.py']
-
-
-def not_cythonized(tup):
-    (package, module, filepath) = tup
-    return any(
-        fnmatch.fnmatchcase(filepath, pat=pattern) for pattern in cython_excludes
-    ) or not any(
-        fnmatch.fnmatchcase(filepath, pat=pattern)
-        for ext in extensions
-        for pattern in ext.sources
-    )
-
-
-class build_py(build_py_orig):
-    def find_modules(self):
-        modules = super().find_modules()
-        return list(filter(not_cythonized, modules))
-
-    def find_package_modules(self, package, package_dir):
-        modules = super().find_package_modules(package, package_dir)
-        return list(filter(not_cythonized, modules))
 
 setup(
     name='fin_benefits',
